a3851e80589b61244fc8bd6cff9702dca9bb6c5f
coordinate <luliyucoordinate@outlook.com>
2018-11-18 19:15:37 +0800

    remove vscode

--- 6_3/6_3/Person.cpp
+++
@@ -1,8 +0,0 @@
-#include "Person.h"
-
-std::ostream & operator<<(std::ostream & out, const Person & person)
-{
-	// TODO: 在此处插入 return 语句
-	std::cout << "Person's name: " << person.name << " Person's hair color: " << person.hair << std::endl;
-	return out;
-}
